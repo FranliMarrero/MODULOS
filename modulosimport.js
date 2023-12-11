916bf0b9b45a2a654bd12c6bc7caffb3d282f58b
Franli Marrero <franlimarrero17@gmail.com>
2023-12-11 18:59:03 -0500

    e

---
+++ modulosimport.js
@@ -0,0 +1,11 @@
+//FUNCION QUE MUESTRA LOS DIEZ PRIMEROS NÙMEROS//
+
+function contar(){
+    for(let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+//EXPORTAMOS LA FUNCIÓN//
+
+export {contar};
